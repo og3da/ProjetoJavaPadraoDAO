4edef9d631179c8bead1834a36dafcc49a1d1dff
og3da <rapha488@gmail.com>
2024-01-11 09:24:59 -0300

    DepartmentDao implementation and test

--- ProjetoJavaDAO/src/model/dao/impl/DepartmentDaoJDBC.java
+++ ProjetoJavaDAO/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -2,7 +2,7 @@ package model.dao.impl;
 
 import db.DB;
 import db.DbException;
-import model.dao.SellerDao;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,35 +12,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SellerDaoJDBC implements SellerDao {
-    /*
-    CLASSE P/ IMPLEMENTAÇÃO DA INTERFACE - SellerDao
-     */
-
+public class DepartmentDaoJDBC implements DepartmentDao {
     private Connection conn;
 
-    public SellerDaoJDBC(Connection conn) {
+    public DepartmentDaoJDBC(Connection conn) {
         this.conn = conn;
     }
 
+
     @Override
-    public void insert(Seller obj) {
+    public void insert(Department obj) {
         PreparedStatement st = null; // var p/consulta SQL
 
         try {
             conn.setAutoCommit(false); // desativando auto confirmação de transações
             // QUERY
-            st = conn.prepareStatement("INSERT INTO seller\n" +
-                            "(Name, Email, BirthDate, BaseSalary, DepartmentId) \n" +
+            st = conn.prepareStatement("INSERT INTO department\n" +
+                            "(Name) \n" +
                             "VALUES \n" +
-                            "(?, ?, ?, ?, ?)",
+                            "(?)",
                     Statement.RETURN_GENERATED_KEYS);
             // VALUES
             st.setString(1, obj.getName());
-            st.setString(2, obj.getEmail());
-            st.setDate(3, obj.getBirthDate());
-            st.setDouble(4, obj.getBaseSalary());
-            st.setInt(5, obj.getDepartment().getId());
 
             int rowsAffected = st.executeUpdate();
             if (rowsAffected > 0) {
@@ -68,22 +61,18 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public void update(Seller obj) {
+    public void update(Department obj) {
         PreparedStatement st = null; // var p/consulta SQL
 
         try {
             conn.setAutoCommit(false); // desativando auto confirmação de transações
             // QUERY
-            st = conn.prepareStatement("UPDATE seller \n" +
-                    "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? \n" +
+            st = conn.prepareStatement("UPDATE department \n" +
+                    "SET Name = ? \n" +
                     "WHERE Id = ?");
             // VALUES
             st.setString(1, obj.getName());
-            st.setString(2, obj.getEmail());
-            st.setDate(3, obj.getBirthDate());
-            st.setDouble(4, obj.getBaseSalary());
-            st.setInt(5, obj.getDepartment().getId());
-            st.setInt(6, obj.getId());
+            st.setInt(2, obj.getId());
 
             int rowsAffected = st.executeUpdate();
             if (rowsAffected <= 0) {
@@ -111,7 +100,7 @@ public class SellerDaoJDBC implements SellerDao {
         try {
             conn.setAutoCommit(false); // desativando auto confirmação de transações
             // QUERY
-            st = conn.prepareStatement("DELETE FROM seller \n" +
+            st = conn.prepareStatement("DELETE FROM department \n" +
                             "WHERE Id = ?",
                     Statement.RETURN_GENERATED_KEYS);
             // VALUES
@@ -135,22 +124,20 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public Seller findById(Integer id) {
+    public Department findById(Integer id) {
         PreparedStatement st = null; // var p/consulta SQL
         ResultSet rs = null; // var p/resultado da consulta
 
         try {
-            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName \n" +
-                    "FROM seller INNER JOIN department \n" +
-                    "ON seller.DepartmentId = department.Id \n" +
-                    "WHERE seller.Id = ?");
+            st = conn.prepareStatement("SELECT *,department.Name as DepName \n" +
+                    "FROM department \n" +
+                    "WHERE department.Id = ?");
             st.setInt(1, id);
 
             rs = st.executeQuery();
             if (rs.next()) {
                 Department dep = instantiateDepartment(rs);
-                Seller seller = instantiateSeller(rs, dep);
-                return seller;
+                return dep;
             }
             return null;
         } catch (SQLException e) {
@@ -162,67 +149,28 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public List<Seller> findByDepartment(Department department) {
+    public List<Department> findAll() {
         PreparedStatement st = null; // var p/consulta SQL
         ResultSet rs = null; // var p/resultado da consulta
 
         try {
-            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName \n" +
-                    "FROM seller INNER JOIN department \n" +
-                    "ON seller.DepartmentId = department.Id\n" +
-                    "WHERE DepartmentId = ?\n" +
-                    "ORDER BY Name");
-            st.setInt(1, department.getId());
-
-            rs = st.executeQuery();
-
-            List<Seller> list = new ArrayList<>(); // lista p/armazenar retorno da consulta de vendedores
-            Map<Integer, Department> map = new HashMap<>(); // map p/nao ter repetição na instanciação do departamento
-
-            while (rs.next()) {
-                Department dep = map.get(rs.getInt("DepartmentId"));
-                if (dep == null) {
-                    dep = instantiateDepartment(rs);
-                    map.put(rs.getInt("DepartmentId"), dep);
-                }
-
-                Seller seller = instantiateSeller(rs, dep);
-                list.add(seller);
-            }
-            return list;
-        } catch (SQLException e) {
-            throw new DbException(e.getMessage());
-        } finally {
-            DB.closeResultSet(rs);
-            DB.closeStatement(st);
-        }
-    }
-
-    @Override
-    public List<Seller> findAll() {
-        PreparedStatement st = null; // var p/consulta SQL
-        ResultSet rs = null; // var p/resultado da consulta
-
-        try {
-            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName \n" +
-                    "FROM seller INNER JOIN department \n" +
-                    "ON seller.DepartmentId = department.Id\n" +
+            st = conn.prepareStatement("SELECT department.*,department.Name as DepName \n" +
+                    "FROM department \n" +
                     "ORDER BY Id");
 
             rs = st.executeQuery();
 
-            List<Seller> list = new ArrayList<>(); // lista p/armazenar retorno da consulta de vendedores
+            List<Department> list = new ArrayList<>(); // lista p/armazenar retorno da consulta de vendedores
             Map<Integer, Department> map = new HashMap<>(); // map p/nao ter repetição na instanciação do departamento
 
             while (rs.next()) {
-                Department dep = map.get(rs.getInt("DepartmentId"));
+                Department dep = map.get(rs.getInt("Id"));
                 if (dep == null) {
                     dep = instantiateDepartment(rs);
-                    map.put(rs.getInt("DepartmentId"), dep);
+                    map.put(rs.getInt("Id"), dep);
                 }
 
-                Seller seller = instantiateSeller(rs, dep);
-                list.add(seller);
+                list.add(dep);
             }
             return list;
         } catch (SQLException e) {
@@ -235,19 +183,8 @@ public class SellerDaoJDBC implements SellerDao {
 
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
         Department dep = new Department();
-        dep.setId(rs.getInt("DepartmentId"));
+        dep.setId(rs.getInt("Id"));
         dep.setName(rs.getString("DepName"));
         return dep;
-    }
-
-    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
-        Seller obj = new Seller();
-        obj.setId(rs.getInt("Id"));
-        obj.setName(rs.getString("Name"));
-        obj.setEmail(rs.getString("Email"));
-        obj.setBaseSalary(rs.getDouble("BaseSalary"));
-        obj.setBirthDate(rs.getDate("BirthDate"));
-        obj.setDepartment(dep);
-        return obj;
     }
 }
